8b87d77b7f6349070a99fffea8069026f8011bf3
Adriansaveli <56266614+Adriansaveli@users.noreply.github.com>
2019-11-04 20:09:43 +0100

    Cirujano extends de Empleado.

--- Clinica-Dental/src/clinica/Cirujano.java
+++ Clinica-Dental/src/clinica/Cirujano.java
@@ -9,11 +9,11 @@ package clinica;
  * @version 2.0
  * @author Mar Santín
  */
-public class Cirujano {
+public class Cirujano extends Empleado{
     private String especialidades;//Esto deberia implementarse en un lista de Especialidad. 
 
     public Cirujano() {
-        
+        super();
     }
 
     public Cirujano(String especialidades) {
@@ -39,13 +39,16 @@ public class Cirujano {
     /*
     Método toString de la clase Cirujano
     */
+
     @Override
     public String toString() {
         return "Cirujano{" + "especialidades=" + especialidades + '}';
     }
+    
     /*
     Método que devuelve los atributos de la clase separados por '|'
     */
+    @Override
     public String data() {
         return getEspecialidades();
     }
