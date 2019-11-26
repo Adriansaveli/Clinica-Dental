df4b5981c386278ee088c3e462ee7336644b3a7c
Marsan08 <marsan97.ms@gmail.com>
2019-11-26 18:01:31 +0100

    Commit Mar Santin Realizacion de el metodo nuevoclase en la clase tratamiento y en la clase medicamento, esta ultima cuenta con un error y no esta terminada.

--- Clinica-Dental/src/clinica/Medicamento.java
+++ Clinica-Dental/src/clinica/Medicamento.java
@@ -5,6 +5,7 @@
  */
 package clinica;
 import java.util.ArrayList;
+import java.util.Scanner;
 /**
  * version 3.0
  * @author DAW109
@@ -21,7 +22,8 @@ public class Medicamento {
     // Principio activo del medicamento
     private int dosisMaxDiaria;
     // Dosis máxima diaria en mg puesto, por tanto tiene que ser un int porque debe ser un numero entero, NO PUEDE SER VALOR 0
-
+    private ArrayList<Cita> citas;
+    
     public Medicamento() {
     }
 
@@ -71,6 +73,16 @@ public class Medicamento {
         this.dosisMaxDiaria = dosisMaxDiaria;
     }
 
+    public ArrayList<Cita> getCitas() {
+        return citas;
+    }
+
+    public void setCitas(ArrayList<Cita> citas) {
+        this.citas = citas;
+    }
+    
+   
+
     @Override
     public String toString() {
         return "Medicamento{" + "idMedicamento=" + id + ", nombre=" + nombre + ", principioActivo=" + principioActivo + ", dosisMaxDiaria=" + dosisMaxDiaria + '}';
@@ -90,5 +102,69 @@ public class Medicamento {
     public Medicamento getMedicamentoById (long id){
         Medicamento m = new Medicamento();
          return m;
+    }
+    
+    
+    public static Medicamento nuevoMedicamento(){
+        
+        Medicamento m = new Medicamento();
+        Scanner in = new Scanner (System.in);
+        boolean correcto;
+       
+        do {
+            //*nombre principioActivo DosisMaxDairia
+            System.out.println("Introduzca el nombre del medicamento:");
+            String nmed = in.nextLine();
+            m.setNombre (med);
+            
+            System.out.println("Introduzca el principio activo del medicamento: ");
+            String prina = in.nextLine();
+            m.setPrincipioActivo(prina);
+            
+            System.out.println("Introduza la dosis maxima diaria en mg:");
+            String dmaxd = in.nextLine();
+            m.setDosisMaxDiaria(dmaxd);
+            
+            ArrayList <Cita> citas = new ArrayList();
+            System.out.println("¿Quieres introducir el numero de citas? ");
+            boolean resp = Utilidades.leerBoleano();
+            if(resp){
+               boolean resp2;
+                do {
+                   Cita c = Cita.nuevoCita();
+                   citas.add(c);
+                   System.out.println("¿Quiere introducir otra cita?");
+                   resp2 =Utilidades.leerBoleano();
+                }
+                while(resp2);    
+                
+                m.setCita (citas);
+            }
+            
+            Alergia alergias = nuevoAlergia();
+           //* System.out.println("Introduzca la alergia: ");
+           //* String aler = in.nextLine();
+            //*m.setAlergia(aler);
+            
+            System.out.println("Los datos introducidos son: "+m);
+            System.out.println("¿Son correctos los datos introducidos?");
+            correcto = Utilidades.leerBoleano();
+        }
+        while (!correcto);
+        
+        
+        return m;
+    }
+
+    private void setDosisMaxDiaria(String dmaxd) {
+        
+    }
+
+    private void setCita(ArrayList<Cita> citas) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void setAlergia(String aler) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
